35131ab43a7cbaba4005800516d550115045bd74
diemnh <nguyen.hong.diem@sun-asterisk.com>
2022-12-20 14:46:04 +0000

    kadai1

---
+++ kadai1.rb
@@ -0,0 +1,18 @@
+class Menu
+ attr_accessor :name
+ attr_accessor :price
+  # get_total_priceメソッドを定義してください
+
+
+
+  
+   return "#{self.name} #{total_price}vnd"
+ end
+end
+
+menu1 = Menu.new
+menu1.name = "Phở"
+menu1.price = 30000
+
+# menu1に対してget_total_priceメソッドを呼び出してください
+
