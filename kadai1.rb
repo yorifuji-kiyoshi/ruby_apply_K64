27495b1ee72ff680a2fc99dda8985af2811e320b
hotuanhung <hungchv16@gmail.com>
2023-01-04 19:16:56 +0700

    kadai1

--- kadai1.rb
+++ kadai1.rb
@@ -2,17 +2,19 @@ class Menu
  attr_accessor :name
  attr_accessor :price
   # get_total_priceメソッドを定義してください
-
-
-
-  
-   return "#{self.name} #{total_price}vnd"
+   def get_total_price(count)
+    sum = @price *count
+    if count>3 
+      sum = sum-10000
+    end
+   return "#{self.name} #{sum}vnd"
  end
 end
 
 menu1 = Menu.new
 menu1.name = "Phở"
 menu1.price = 30000
+puts menu1.get_total_price(4)
 
 # menu1に対してget_total_priceメソッドを呼び出してください
 
