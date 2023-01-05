dfbc415ef26f7f94020814d2773603b7ea61a050
Replit user <>
2023-01-05 13:46:48 +0000

    add kadai1234.rb

--- kadai1.rb
+++ kadai1.rb
@@ -1,18 +1,16 @@
 class Menu
  attr_accessor :name
  attr_accessor :price
-  # get_total_priceメソッドを定義してください
-
-
-
   
-   return "#{self.name} #{total_price}vnd"
- end
-end
+  def get_total_price(number)
+	total_price = price * number
+   total_price -= 10000 if number >= 3 
+	"#{name} #{total_price}vnd"
+	end
+	end
 
 menu1 = Menu.new
 menu1.name = "Phở"
 menu1.price = 30000
 
-# menu1に対してget_total_priceメソッドを呼び出してください
-
+puts menu1.get_total_price(10)
