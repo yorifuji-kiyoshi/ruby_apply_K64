fc483bbdb272347fdc3f251938bf30198ac46795
Replit user <>
2023-01-11 02:02:07 +0000

    kadai1.rb kadai2.rb kadai3.rb kadai4.rb

---
+++ kadai2.rb
@@ -0,0 +1,24 @@
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+  # get_total_priceメソッドを定義してください
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def info
+    puts "#{name} #{price}vnd"
+  end
+end
+
+
+menu1 = Menu.new('Phở', 30000)
+menu2 = Menu.new('Bún chả', 40_000)
+menu3 = Menu.new('Bánh mì', 20_000)
+
+menus = [menu1, menu2, menu3]
+menus.each do |menu|
+  menu.info
+end
