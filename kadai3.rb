bde35f742b7f99015139647544ed0fbce150102c
Replit user <>
2023-01-11 14:03:57 +0000

    kadai1234

---
+++ kadai3.rb
@@ -0,0 +1,38 @@
+class Menu
+  attr_accessor :name, :price
+  def initialize(name, price)
+    self.name = name
+    self.price = price
+    def info
+      puts "#{name} #{price} vnd"
+    end
+  end
+end
+
+class Drink < Menu
+  attr_accessor :size
+  def initialize(name, price, size)
+    super(name, price)
+    self.size = size
+    def info
+      puts "#{name} #{price} vnd (#{size})"
+    end
+  end
+end
+
+drink1 = Drink.new('Trà', 5000, 'S サイズ')
+drink2 = Drink.new('Trà', 10_000, 'M サイズ')
+menu1 = Menu.new('Phở', 30_000)
+menu2 = Menu.new('Bún chả', 40_000)
+menu3 = Menu.new('Bánh mì', 20_000)
+
+menus = [menu1, menu2, menu3, drink1, drink2]
+
+menus.each do |menu|
+  # if menu.is_a?(Drink)
+  # puts "#{menu.name} #{menu.price} vnd (#{menu.size})"
+  menu.info
+  # else
+  # puts "#{menu.name} #{menu.price} vnd"
+  # end
+end
