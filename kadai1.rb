e09327de1e271d4a9203586863e8d938098e4303
ducthien1005@gmail.com <ducthien1005@gmail.com>
2023-01-11 07:33:51 +0700

    kadai1234

--- kadai1.rb
+++ kadai1.rb
@@ -1,18 +1,19 @@
 class Menu
- attr_accessor :name
- attr_accessor :price
+  attr_accessor :name
+  attr_accessor :price
   # get_total_priceメソッドを定義してください
-
-
-
-  
-   return "#{self.name} #{total_price}vnd"
- end
+  def get_total_price(count)
+    total_price = self.price * count
+    if count >= 3
+      total_price -= 10000
+    end
+    return "#{self.name} #{total_price}vnd"
+  end
 end
 
 menu1 = Menu.new
 menu1.name = "Phở"
 menu1.price = 30000
-
+puts menu1.get_total_price(4)
 # menu1に対してget_total_priceメソッドを呼び出してください
 
---
+++ kadai4.rb
@@ -0,0 +1,35 @@
+class Bingo
+  attr_accessor :b
+  attr_accessor :i
+  attr_accessor :n
+  attr_accessor :g
+  attr_accessor :o
+
+  def initialize
+    @b = (01..15).to_a.sample(5)
+    @i = (16..30).to_a.sample(5)
+    @n = (31..45).to_a.sample(5)
+    @n[2] = '  '
+    @g = (46..60).to_a.sample(5)
+    @o = (61..75).to_a.sample(5)
+  end
+
+  def showBingoSheet
+    puts " B| I| N| G| O"
+    # puts "#{@b}"
+    for a in 0..4 do
+      row = []
+      [b,i,n,g,o].each do |character|
+        row.push(character[a])
+      end
+        print "#{format('%2s',row[0])}|"
+        print "#{format('%2s',row[1])}|"
+        print "#{format('%2s',row[2])}|"
+        print "#{format('%2s',row[3])}|"
+        puts "#{format('%2s',row[4])}|"
+    end
+  end
+end
+
+sheet = Bingo.new
+sheet.showBingoSheet
